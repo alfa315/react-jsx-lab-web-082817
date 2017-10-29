af83ba1a004519de49897de4f009dd816768335d
github username <github email address>
2017-10-29 14:48:23 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import RegistrationForm from './components/RegistrationForm';
-import Webpage from './components/Webpage';
+import Webpage from './components/Webpage'
 
 ReactDOM.render(
   (
@@ -11,5 +11,5 @@ ReactDOM.render(
       <Webpage />
     </div>
   ),
-  document.getElementById('global')
+  document.getElementById('main')
 );
